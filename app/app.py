1bc697f27b7eeac888199363e923c9b926fe99bb
AJWelsh <24477640+AJWelsh@users.noreply.github.com>
2019-01-04 12:46:10 +0000

    Simple Flask app with basic auth.
    	- currently prints out the sent request

---
+++ app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, json, Response
+from functools import wraps
+
+app = Flask(__name__)
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/predict', methods = ['POST'])
+@requires_auth
+def predict():
+	result = json.dumps(request.json)
+	response = Response(result, status=200)
+	return response
